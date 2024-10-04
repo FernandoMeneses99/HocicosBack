5d09d556a3c096723248cfbdda4681842638a453
Omar Meneses <fernandomenesesda@gmail.com>
2024-10-03 21:43:31 -0500

    Se crearon los controllers de los modelos

---
+++ Controllers/ClienytesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HocicosBack.Controllers
+{
+    public class ClienytesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Controllers/EnviosController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HocicosBack.Controllers
+{
+    public class EnviosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
