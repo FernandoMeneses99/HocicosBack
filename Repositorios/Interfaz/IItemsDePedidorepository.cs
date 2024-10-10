49a45652c6100752b06af57c2667f8599e1f567f
SOFIAREP <131830444+ZSofiaTorov@users.noreply.github.com>
2024-10-09 19:29:32 -0500

    Ajustes

--- Repositorios/Interfaz/IItemsDePedidorepository.cs
+++ Repositorios/Interfaz/IItemsDePedidorepository.cs
@@ -4,12 +4,10 @@ namespace HocicosBack.Repositorios.Interfaz
 {
     public interface IItemsDePedidoRepository
     {
-        Task<List<ItemsDePedido>> GetItemsDePedidos();
-        Task<bool> PostItemsDePedido (ItemsDePedido itemsDePedido);
-        Task<ItemsDePedido> GetItemsDePedidoByID(int id);
+        Task<List<ItemsDePedido>> GetItemsDePedido();
+        Task<bool> PostItemsDePedido(ItemsDePedido itemsDePedido);
+        Task<ItemsDePedido> GetItemDePedidoByID(int id); 
         Task<bool> UpdateItemsDePedido(ItemsDePedido itemsDePedido);
-        Task<bool> DeleteItemsDePedido (int id);
-
+        Task<bool> DeleteItemsDePedido(int id);
     }
 }
-
